5ac441bb9e11c1bd69302f98da8538fba24635c5
coding-guys99 <tnuocsa2019@gmail.com>
2026-08-29 23:09:54 +0800

    Combine 100 verified Borneo opportunity records

---
+++ data/opportunities.ts
@@ -0,0 +1,6 @@
+import sabah from './sabah.json';
+import sarawak from './sarawak.json';
+import brunei from './brunei.json';
+
+const opportunities=[...sabah,...sarawak,...brunei];
+export default opportunities;
